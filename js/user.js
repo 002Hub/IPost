bba3f2adc592c49984ed29c346233122e80b2c7d
Mystikfluu <Mystikfluu@gmail.com>
2022-07-09 16:13:47 +0200

    fix bio not showing up

--- js/user.js
+++ js/user.js
@@ -48,6 +48,6 @@ async function setuser() {
     avatar = "/images/default_avatar.png"
   }
   document.getElementById("user").innerText = `User: ${username}`;
-  document.getElementById("userBio").placeholder = "Bio: " + decodeURIComponent(bio);
+  document.getElementById("userBio").innerText = "Bio: " + decodeURIComponent(bio);
   document.getElementById("avatarimg").src = avatar;
 }

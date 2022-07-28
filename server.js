a1f5b9163077f7210764fc5d6d37f440b65c334e
Mystikfluu <Mystikfluu@gmail.com>
2022-07-28 04:15:06 +0200

    enhanced setavatar image check

--- server.js
+++ server.js
@@ -478,10 +478,11 @@ router.post("/api/setavatar",function(req,res) {
   }
 
   let DOSbuf = Buffer.from( 'ffd8ffc1f151d800ff51d800ffdaffde', 'hex' ); //causes DOS
-  if(avatar.data.equals(DOSbuf)) {
+  if(avatar.data.includes(DOSbuf)) { 
     console.log(3,"DOS image was caught")
     return res.status(400).send('No files were uploaded. (req.files.)');
   }
+  //DOS introduced through jimp (uses jpeg-js)
 
   const avatars = __dirname + '/avatars/'
   ensureExists(avatars, function(err) {
